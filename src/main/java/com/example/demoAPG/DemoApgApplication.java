ab47414525d51a0fb7e36c93c49cf8249d1d1ceb
Andrey Zemlyachev <kvintilian@gmail.com>
2023-01-29 22:46:19 +0300

    Initial project

---
+++ src/main/java/com/example/demoAPG/DemoApgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoAPG/DemoApgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
